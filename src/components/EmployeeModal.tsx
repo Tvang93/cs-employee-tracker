17b8557aedda5cf653610239315ff042a7e9cc95
Tvang93 <vangt39@gmail.com>
2025-05-22 09:04:03 -0700

    employee modal working with dropdown so users cant create a different job title

--- src/components/EmployeeModal.tsx
+++ src/components/EmployeeModal.tsx
@@ -1,250 +1,257 @@
-'use client'
+"use client";
 
-import { Employee } from '@/lib/interfaces/interfaces'
-import { addEmployee, updateEmployee } from '@/lib/services/employee-service';
-import React, { useEffect, useState } from 'react'
-import { Button } from './ui/button';
-import { FaPlus } from 'react-icons/fa';
-import { Label } from './ui/label';
-import { Input } from './ui/input';
-import { Popover, PopoverContent, PopoverTrigger } from './ui/popover';
-import { Calendar } from './ui/calendar';
-import { CalendarIcon } from 'lucide-react';
-import { cn } from '@/lib/utils';
-import { Dialog, DialogClose, DialogContent, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from './ui/dialog';
-import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from './ui/select';
-
+import { Employee } from "@/lib/interfaces/interfaces";
+import { addEmployee, updateEmployee } from "@/lib/services/employee-service";
+import React, { useEffect, useState } from "react";
+import { Button } from "./ui/button";
+import { FaPlus } from "react-icons/fa";
+import { Label } from "./ui/label";
+import { Input } from "./ui/input";
+import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
+import { Calendar } from "./ui/calendar";
+import { CalendarIcon } from "lucide-react";
+import { cn } from "@/lib/utils";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog";
 
 // Valid values for type: "Add" & "Edit"
-const EmployeeModal = ({ type, employee, refreshEmployees }: { type: 'Add' | 'Edit', employee: Employee | null, refreshEmployees: () => Promise<void> }) => {
+const EmployeeModal = ({
+  type,
+  employee,
+  refreshEmployees,
+}: {
+  type: "Add" | "Edit";
+  employee: Employee | null;
+  refreshEmployees: () => Promise<void>;
+}) => {
+  // useStates
+  const [openModal, setOpenModal] = useState(false);
+  const [employeeToChange, setEmployeeToChange] = useState<Employee>({
+    id: 0,
+    name: "",
+    jobTitle: "",
+    hireDate: "",
+  });
 
-    // useStates
-    const [openModal, setOpenModal] = useState(false);
-    const [employeeToChange, setEmployeeToChange] = useState<Employee>({
+  const [token, setToken] = useState("");
+
+  const disableBtn =
+    employeeToChange.name.trim() == "" ||
+    (employeeToChange.jobTitle.trim() == "" && employeeToChange.hireDate == "");
+
+  // Modal Functions
+  const onOpenModal = () => {
+    if (type === "Edit") {
+      setEmployeeToChange(employee as Employee);
+    }
+
+    setOpenModal(true);
+  };
+
+  const onCloseModal = () => {
+    setOpenModal(false);
+    setEmployeeToChange({ id: 0, name: "", jobTitle: "", hireDate: "" });
+  };
+
+  // Change employee functions
+  const handleEmployeeToChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
+    setEmployeeToChange({
+      ...employeeToChange,
+      [e.target.id]: e.target.value,
+    });
+    console.log(e.target.value);
+  };
+
+  const handleEmployeeToChangeHireDate = (date: string) => {
+    setEmployeeToChange({
+      ...employeeToChange,
+      hireDate: date,
+    });
+  };
+
+  // Date functions
+  const formatDateForInput = (date: string) => {
+    if (!date) return undefined;
+
+    const [year, month, day] = date.toString().split("-").map(Number);
+    return new Date(year, month - 1, day);
+  };
+
+  const formatDateFromInput = (date: Date | undefined) => {
+    if (!date) return "";
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    const day = String(date.getDate()).padStart(2, "0");
+
+    return `${year}-${month}-${day}`;
+  };
+
+  // Add & Edit function
+  const handleEmployee = async () => {
+    try {
+      const employeeWithChanges = {
+        ...employeeToChange,
+        name: employeeToChange.name.trim(),
+        jobTitle: employeeToChange.jobTitle.trim(),
+      };
+
+      if (type === "Add") {
+        if (await addEmployee(token, employeeWithChanges)) {
+          await refreshEmployees();
+        }
+      } else {
+        if (await updateEmployee(token, employeeWithChanges)) {
+          await refreshEmployees();
+        }
+      }
+
+      setEmployeeToChange({
         id: 0,
         name: "",
         jobTitle: "",
-        hireDate: ""
-    });
+        hireDate: "",
+      });
+    } catch (error) {
+      console.log("error", error);
+    }
 
-    const [token, setToken] = useState('');
+    onCloseModal();
+  };
 
-    const disableBtn =
-        employeeToChange.name.trim() != "" ||
-        employeeToChange.jobTitle.trim() != "" &&
-        employeeToChange.hireDate != "";
-
-    // Modal Functions
-    const onOpenModal = () => {
-        if (type === "Edit") {
-            setEmployeeToChange(employee as Employee);
-        }
-
-        setOpenModal(true);
+  useEffect(() => {
+    const handleToken = async () => {
+      if (localStorage.getItem("user")) {
+        setToken(await JSON.parse(localStorage.getItem("user")!).token);
+      }
+      if (sessionStorage.getItem("user")) {
+        setToken(await JSON.parse(sessionStorage.getItem("user")!).token);
+      }
     };
 
-    const onCloseModal = () => {
-        setOpenModal(false);
-        setEmployeeToChange({ id: 0, name: "", jobTitle: "", hireDate: ""});
-    };
+    handleToken();
+  }, []);
 
-    // Change employee functions
-    const handleEmployeeToChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
-        setEmployeeToChange({
-            ...employeeToChange,
-            [e.target.id]: e.target.value,
-        });
-    };
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        {/* <Button variant="outline">Edit Profile</Button> */}
+        <Button
+          color="success"
+          className={`${
+            type === "Add" ? "flex items-center gap-1" : ""
+          } hover:cursor-pointer`}
+          onClick={onOpenModal}
+        >
+          {type === "Add" ? <FaPlus className="mt-[0.2rem]" /> : "Edit"}
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="w-[40rem]">
+        <DialogHeader className="pb-3">
+          <DialogTitle>
+            {type === "Add"
+              ? "Add New Employee"
+              : "Update Employee Information"}
+          </DialogTitle>
+        </DialogHeader>
+        <div className="grid grid-cols-2 gap-3 min-h-[30rem]">
+          <div>
+            <div className="pb-5">
+              <div className="mb-2 block">
+                <Label htmlFor="name">Employee name</Label>
+              </div>
+              <Input
+                id="name"
+                value={employeeToChange.name}
+                onChange={handleEmployeeToChange}
+              />
+            </div>
+            <div>
+              <div className="mb-2 block">
+                <Label htmlFor="jobTitle">Job title</Label>
+              </div>
 
-    const handleEmployeeToChangeHireDate = (date: string) => {
-        setEmployeeToChange({
-            ...employeeToChange,
-            hireDate: date,
-        });
-    };
-
-    // Date functions
-    const formatDateForInput = (date: string) => {
-        if (!date) return undefined;
-
-        const [year, month, day] = date.toString().split("-").map(Number);
-        return new Date(year, month - 1, day);
-    };
-
-    const formatDateFromInput = (date: Date | undefined) => {
-        if (!date) return "";
-        const year = date.getFullYear();
-        const month = String(date.getMonth() + 1).padStart(2, "0");
-        const day = String(date.getDate()).padStart(2, "0");
-
-        return `${year}-${month}-${day}`;
-    };
-
-    // Add & Edit function
-    const handleEmployee = async () => {
-        try {
-            const employeeWithChanges = {
-                ...employeeToChange,
-                name: employeeToChange.name.trim(),
-                jobTitle: employeeToChange.jobTitle.trim(),
-            };
-
-            if (type === "Add") {
-                if (await addEmployee(token, employeeWithChanges)) {
-                    await refreshEmployees();
-                }
-            } else {
-                if (await updateEmployee(token, employeeWithChanges)) {
-                    await refreshEmployees();
-                }
-            }
-
-            setEmployeeToChange({
-                id: 0,
-                name: "",
-                jobTitle: "",
-                hireDate: "",
-            });
-        } catch (error) {
-            console.log("error", error);
-        }
-
-        onCloseModal();
-    };
-
-    useEffect(() => {
-        const handleToken = async () => {
-            if (localStorage.getItem('user')) {
-                setToken(await JSON.parse(localStorage.getItem('user')!).token);
-            }
-            if (sessionStorage.getItem('user')) {
-                setToken(await JSON.parse(sessionStorage.getItem('user')!).token);
-            }
-        }
-
-        handleToken();
-    }, []);
-
-
-    return (
-        <Dialog >
-            <DialogTrigger asChild>
-                {/* <Button variant="outline">Edit Profile</Button> */}
-                <Button
-                    color="success"
-                    className={`${type === "Add" ? "flex items-center gap-1" : ""} hover:cursor-pointer`}
-                    onClick={onOpenModal}
+              <select
+                id="jobTitle"
+                className="text-sm border rounded p-1 hover:cursor-pointer"
+                value={employeeToChange.jobTitle}
+                onChange={handleEmployeeToChange}
+              >
+                <option className="hover:cursor-pointer" value="" disabled>
+                  Job title
+                </option>
+                <option className="cursor-pointer" value="Customer Support">
+                  Customer Support
+                </option>
+                <option
+                  className="cursor-pointer"
+                  value="IT Support Specialist"
                 >
-                    {type === "Add" ? <FaPlus className="mt-[0.2rem]" /> : "Edit"}
+                  IT Support Specialist
+                </option>
+                <option className="cursor-pointer" value="Software Engineer">
+                  Software Engineer
+                </option>
+              </select>
+            </div>
+          </div>
+          <div>
+            <div className="mb-2 block">
+              <Label>Date hired</Label>
+            </div>
+            <Popover>
+              <PopoverTrigger asChild>
+                <Button
+                  variant={"outline"}
+                  className={cn(
+                    "w-full justify-start text-left font-normal hover:cursor-pointer",
+                    !employeeToChange.hireDate && "text-muted-foreground"
+                  )}
+                >
+                  <CalendarIcon />
+                  {employeeToChange.hireDate ? (
+                    employeeToChange.hireDate
+                  ) : (
+                    <span>Pick a date</span>
+                  )}
                 </Button>
-            </DialogTrigger>
-            <DialogContent className='w-[40rem]'>
-                <DialogHeader className='pb-3'>
-                    <DialogTitle>
-                        {type === "Add"
-                            ? "Add New Employee"
-                            : "Update Employee Information"}
-                    </DialogTitle>
-                </DialogHeader>
-                <div className="grid grid-cols-2 gap-3 min-h-[30rem]">
-                    <div>
-                        <div className='pb-5'>
-                            <div className="mb-2 block">
-                                <Label htmlFor="name">Employee name</Label>
-                            </div>
-                            <Input
-                                id="name"
-                                value={employeeToChange.name}
-                                onChange={handleEmployeeToChange}
-                            />
-                        </div>
-                        <div>
-                            <div className="mb-2 block">
-                                <Label htmlFor="jobTitle">Job title</Label>
-                            </div>
+              </PopoverTrigger>
+              <PopoverContent className="w-auto p-0">
+                <Calendar
+                  mode="single"
+                  selected={formatDateForInput(employeeToChange.hireDate)}
+                  onSelect={(e) =>
+                    handleEmployeeToChangeHireDate(formatDateFromInput(e))
+                  }
+                  initialFocus
+                />
+              </PopoverContent>
+            </Popover>
+          </div>
+        </div>
+        <DialogFooter>
+          <DialogClose asChild>
+            <Button
+              className="hover:cursor-pointer"
+              onClick={handleEmployee}
+              color="success"
+              disabled={disableBtn}
+            >
+              {type === "Add" ? "Add" : "Update"} Employee
+            </Button>
+          </DialogClose>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
 
-    {/* -------------------- make change here -------------------------- */}
-                        {/* <Select>
-                            <SelectTrigger id="jobTitle" className="hover:cursor-pointer" >
-                                <SelectValue 
-                                onChange={handleEmployeeToChange}
-                                placeholder="Job Title"
-                                />
-                            </SelectTrigger>
-                            <SelectContent>
-                        <SelectGroup>
-                            <SelectLabel>Job Title</SelectLabel>
-                            <SelectItem className='hover:cursor-pointer' value="Customer Support">Customer Support</SelectItem>
-                            <SelectItem className='hover:cursor-pointer' value="IT Support Specialist">IT Support Specialist</SelectItem>
-                            <SelectItem className='hover:cursor-pointer' value="Software Engineer">Software Engineer</SelectItem>
-                        </SelectGroup>
-                    </SelectContent>
-                        </Select> */}
-                            <select
-                            id="jobTitle"
-                            className="ml-3 text-sm border rounded p-1 hover:cursor-pointer"
-                            value={employeeToChange.jobTitle}
-                            onChange={handleEmployeeToChange}
-                        >
-                            <option className='hover:cursor-pointer' value="" disabled>
-                                Job title
-                            </option>
-                            <option className='cursor-pointer' value="Customer Support">Customer Support</option>
-                            <option className='cursor-pointer' value="IT Support Specialist">IT Support Specialist</option>
-                            <option className='cursor-pointer' value="Software Engineer">Software Engineer</option>
-                        </select>
-                            {/* <Input
-                                id="jobTitle"
-                                value={employeeToChange.jobTitle}
-                                onChange={handleEmployeeToChange}
-                            /> */}
-                        </div>
-                    </div>
-                    <div>
-                        <div className="mb-2 block">
-                            <Label>Date hired</Label>
-                        </div>
-                        <Popover>
-                            <PopoverTrigger asChild>
-                                <Button
-                                    variant={"outline"}
-                                    className={cn(
-                                        "w-full justify-start text-left font-normal hover:cursor-pointer",
-                                        !employeeToChange.hireDate && "text-muted-foreground"
-                                    )}
-                                >
-                                    <CalendarIcon />
-                                    {employeeToChange.hireDate ? employeeToChange.hireDate : <span>Pick a date</span>}
-                                </Button>
-                            </PopoverTrigger>
-                            <PopoverContent className="w-auto p-0">
-                                <Calendar
-                                    mode="single"
-                                    selected={formatDateForInput(employeeToChange.hireDate)}
-                                    onSelect={(e) =>
-                                        handleEmployeeToChangeHireDate(formatDateFromInput(e))
-                                    }
-                                    initialFocus
-                                />
-                            </PopoverContent>
-                        </Popover>
-                    </div>
-                </div>
-                <DialogFooter>
-                    <DialogClose asChild>
-                        <Button
-                            className='hover:cursor-pointer'
-                            onClick={handleEmployee}
-                            color="success"
-                            disabled={disableBtn}
-                        >
-                            {type === "Add" ? "Add" : "Update"} Employee
-                        </Button>
-                    </DialogClose>
-                </DialogFooter>
-            </DialogContent>
-        </Dialog>
-    )
-}
-
-export default EmployeeModal
+export default EmployeeModal;
