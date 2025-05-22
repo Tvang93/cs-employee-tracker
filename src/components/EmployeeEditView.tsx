fa7b5028b2f46e3676943867c6b2d40828e480bb
Tvang93 <vangt39@gmail.com>
2025-05-22 15:45:01 -0700

    working on edit functionality

--- src/components/EmployeeEditView.tsx
+++ src/components/EmployeeEditView.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { Employee } from '@/lib/interfaces/interfaces'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Button } from './ui/button'
 import { Input } from './ui/input'
 import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from './ui/select'
@@ -11,17 +11,57 @@ import { CalendarIcon } from 'lucide-react'
 import { Calendar } from './ui/calendar'
 
 const EmployeeEditView = ({ employee, setEdit }: { employee: Employee, setEdit: (value: boolean) => void }) => {
+    const [details, setDetails] = useState<string>("")
+    const [status, setStatus] = useState<string>("")
+      const [employeeToChange, setEmployeeToChange] = useState<Employee>({
+        id: 0,
+        name: "",
+        jobTitle: "",
+        hireDate: "",
+      });
+
+    useEffect(()=>{
+        setEmployeeToChange(employee)
+    }, [])
+
+    useEffect(()=>{
+
+    }, [details])
+
+      const handleEmployeeToChangeHireDate = (date: string) => {
+    setEmployeeToChange({
+      ...employeeToChange,
+      hireDate: date,
+    });
+  };
+
+  // Date functions
+  const formatDateForInput = (date: string) => {
+    if (!date) return undefined;
+
+    const [year, month, day] = date.toString().split("-").map(Number);
+    return new Date(year, month - 1, day);
+  };
+
+  const formatDateFromInput = (date: Date | undefined) => {
+    if (!date) return "";
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    const day = String(date.getDate()).padStart(2, "0");
+
+    return `${year}-${month}-${day}`;
+  };
 
     return (
         <>
             <div>
                 <p className="text-sm font-semibold">Job Title</p>
-                <Input value={employee.jobTitle} />
+                <Input readOnly value={employee.jobTitle} />
             </div>
 
             <div>
                 <p className="text-sm font-semibold">Details</p>
-                <Input value={employee.details || ""} />
+                <Input onChange={(e)=>(setDetails(e.target.value))} />
             </div>
 
             <div>
@@ -50,13 +90,18 @@ const EmployeeEditView = ({ employee, setEdit }: { employee: Employee, setEdit: 
                                     className={cn("w-full justify-start text-left font-normal text-muted-foreground hover:cursor-pointer")}
                                 >
                                     <CalendarIcon />
-                                    <span>Pick a date</span>
+                                    <span>{new Date(employee.hireDate).toLocaleDateString()}</span>
                                 </Button>
                             </PopoverTrigger>
                             <PopoverContent className="w-auto p-0">
                                 <Calendar
                                     mode="single"
-                                    selected={new Date()}
+                                    selected={formatDateForInput(employee.hireDate)}
+                                    // onSelect={(e) => {
+                                    //     console.log("test");
+                                    //     handleEmployeeToChangeHireDate(formatDateFromInput(e))
+                                    //     }
+                                    // }
                                     initialFocus
                                 />
                             </PopoverContent>
--- src/components/EmployeeTable.tsx
+++ src/components/EmployeeTable.tsx
@@ -67,6 +67,7 @@ const EmployeeTable = () => {
     }
   };
 
+  // Function to Chunk Array
 function chunkArray<T>(array: T[], size: number): T[][] {
     return Array.from({ length: Math.ceil(array.length / size) }, (_, i) =>
         array.slice(i * size, i * size + size)
